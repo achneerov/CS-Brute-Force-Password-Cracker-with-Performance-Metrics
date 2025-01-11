6686c00e04ecc3a1ed20718e91f15598ef3fd604
Alexander Chneerov <123044733+achneerov@users.noreply.github.com>
2025-01-11 10:23:37 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;  // Add this namespace
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading;
+
 
 class BruteForcePasswordCracker
 {
